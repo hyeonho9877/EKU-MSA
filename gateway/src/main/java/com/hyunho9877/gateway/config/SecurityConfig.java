a19a2a1971ec75e2e201596f0ee2716bfb4fad78
hyeonho <6190606@naver.com>
2022-10-25 14:08:32 +0900

    add-gateway

---
+++ gateway/src/main/java/com/hyunho9877/gateway/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.hyunho9877.gateway.config;
+
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.WebFilterExchange;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
+import reactor.core.publisher.Mono;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf().disable()
+                .authorizeExchange(exchanges ->
+                        exchanges
+                                .pathMatchers("/actuator/**", "/hello").permitAll()
+                                .anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+}
