c4cb8d1c9e888a4876d7cd938d44fc588ec5e8d6
hyeonho <6190606@naver.com>
2022-10-28 22:11:31 +0900

    account-as-oauth2-resource-server

--- auth/src/main/java/com/hyunho9877/auth/config/SecurityConfig.java
+++ auth/src/main/java/com/hyunho9877/auth/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.hyunho9877.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,7 +18,7 @@ public class SecurityConfig {
                         request
                                 .antMatchers("/api/v1/accounts/registration").permitAll()
                                 .anyRequest().authenticated())
-                .oauth2ResourceServer(Customizer.withDefaults());
+                .oauth2ResourceServer().jwt();
         return http.build();
     }
 }
