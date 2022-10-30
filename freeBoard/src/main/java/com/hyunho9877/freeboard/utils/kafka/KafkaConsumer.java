c2bec69b14e56b91e81b83263a1b231201bb9c81
hyeonho <6190606@naver.com>
2022-10-30 17:33:44 +0900

    kafka-consume-produce

--- freeBoard/src/main/java/com/hyunho9877/freeboard/utils/kafka/KafkaConsumer.java
+++ freeBoard/src/main/java/com/hyunho9877/freeboard/utils/kafka/KafkaConsumer.java
@@ -3,18 +3,19 @@ package com.hyunho9877.freeboard.utils.kafka;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
-@Slf4j
-@Transactional
 public class KafkaConsumer {
     private final String kafkaTopic = "account-freeboard-topic";
+    private final String groupId = "freeboard";
 
-    @KafkaListener(topics = kafkaTopic)
-    public void updateDeletedUserArticle(String kafkaMessage) {
+    @KafkaListener(topics = kafkaTopic, groupId = groupId)
+    public void updateDeletedUserArticle(@Payload String kafkaMessage) {
         log.info("kafka message received : {}", kafkaMessage);
     }
 }
