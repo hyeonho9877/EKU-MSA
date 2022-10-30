c765d3be31f094d073ac02a02b5af568d31d021e
hyeonho <6190606@naver.com>
2022-10-30 17:33:44 +0900

    kafka-consume-produce

--- auth/src/main/java/com/hyunho9877/auth/service/KafkaServiceImpl.java
+++ auth/src/main/java/com/hyunho9877/auth/service/KafkaServiceImpl.java
@@ -6,18 +6,24 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.EnumSet;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class KafkaServiceImpl implements KafkaService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Override
-    public void send(String userId, EnumSet<KafkaTopics> topics) {
-        topics.forEach(topic -> kafkaTemplate.send(topic.getTopicName(), userId));
+    public void send(String username, EnumSet<KafkaTopics> topics) {
+        log.info("user id : {}", username);
+        topics.forEach(topic -> {
+            log.info("topic : {}", topic.getTopicName());
+             kafkaTemplate.send(topic.getTopicName(), "username", username);
+        });
     }
 }
