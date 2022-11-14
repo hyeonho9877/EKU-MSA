7ebf9ebdaa78645475477c747ec568f4e89031e4
hyeonho <6190606@naver.com>
2022-11-14 21:46:59 +0900

    helm-chart

--- auth/src/main/java/com/hyunho9877/auth/feign/KeyCloakFeignClient.java
+++ auth/src/main/java/com/hyunho9877/auth/feign/KeyCloakFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "keycloak-feign-client", url = "http://keycloak:8073/", configuration = FeignConfig.class)
+@FeignClient(name = "keycloak-feign-client", url = "http://keycloak/", configuration = FeignConfig.class)
 public interface KeyCloakFeignClient {
     @PostMapping(value = "/admin/realms/eku/users", consumes = MediaType.APPLICATION_JSON_VALUE)
     void registerUser(@RequestBody KeyCloakUserDto userRepresentation);
