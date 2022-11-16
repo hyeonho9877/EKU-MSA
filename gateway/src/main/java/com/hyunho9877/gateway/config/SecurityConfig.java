0425890b7e630a2ab48c0f519de6f26f78be7f39
hyeonho <6190606@naver.com>
2022-11-16 20:47:17 +0900

    gateway-as-oauth2-resource-server

--- gateway/src/main/java/com/hyunho9877/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/hyunho9877/gateway/config/SecurityConfig.java
@@ -1,17 +1,15 @@
 package com.hyunho9877.gateway.config;
 
-import org.apache.commons.logging.Log;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.WebFilterExchange;
-import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -20,13 +18,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+
+
         http
                 .csrf().disable()
                 .authorizeExchange(exchanges ->
                         exchanges
                                 .pathMatchers("/actuator/**", "/account/api/v1/accounts/registration").permitAll()
                                 .anyExchange().authenticated())
-                .oauth2Login(Customizer.withDefaults());
+                .oauth2ResourceServer().jwt().jwtAuthenticationConverter(grantedAuthoritiesExtractor());
         return http.build();
     }
+
+    Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(converter);
+    }
+
 }
